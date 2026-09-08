93694fda3337510202d8bbbcd75fccabd087e3cc
juliusmwass <juliusmwass100@gmail.com>
2026-09-08 11:14:49 +0300

    chore: initialize Clientor website

---
+++ apps/website/src/routes/AppRoutes.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function Home() {
+  return <h1>Home</h1>;
+}
+
+function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default AppRoutes;
